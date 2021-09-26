0b30f85c606dcc66c0bfd9069b00ee41047a8de6
prince232002 <prince2002gupta@gmail.com>
2021-09-26 23:43:37 +0530

    Gitacora saved 1 changed files

--- dsaa/stl/set/s3.cpp
+++ dsaa/stl/set/s3.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 int main(){
  unordered_set<int> s;
- 
+ int n;
+ cin>>n;
+ for(int i=0; i<n; i++){
+     string str;
+     cin>>str;
+     s.insert(str);
+ }
+ for(auto it:s ){
+ cout<<it<<endl;
+ }
 return 0;
 }
