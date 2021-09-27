34938e94247beb2d6b3a30a18943d02efd510d43
prince232002 <prince2002gupta@gmail.com>
2021-09-28 00:23:32 +0530

    ff

---
+++ dsaa/stl/set/s5.cpp
@@ -0,0 +1,22 @@
+// here in set insert of duplicate is not allowed while in multiset is allowed 
+#include<bits/stdc++.h>
+using namespace std;
+void print(multiset<string> &s){
+    for(string value: s){
+        cout<<value<<endl;
+    }
+}
+int main(){
+ multiset<string> s;
+ s.insert("abc");   //O(log n)
+ s.insert("gss");
+ s.insert("gss");
+ s.insert("fss");
+auto it=s.find("abc");
+if(it!=s.end()){
+    // cout<<(*it);
+    s.erase("abc");
+    print(s);
+}
+return 0;
+}
---
+++ dsaa/stl/set/s6.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+multiset<int> m;
+int t;    //testcase 
+ cin>>t;
+ while (t--){
+     int n,k;    //n-no_of_bags ,k-minutes
+     cin>>n>>k;
+     multiset<long long> bags;
+     for(int i=0; i<n; i++){
+         long long candy_ct;
+         cin>>candy_ct;
+         bags.insert(candy_ct);
+              }
+     long long total_candies=0;
+     for(int i=0; i<k; i++){
+         auto last_it=(--bags.end());
+         long long candy_ct=*last_it;
+          total_candies+=candy_ct;
+         bags.erase(last_it);
+         bags.insert(candy_ct/2);
+    }
+    cout<<total_candies<<endl;
+
+ }
+return 0;
+}
