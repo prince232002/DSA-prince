f8fba0c8c708803cfeccf8e4099e0d0b86b7581b
prince232002 <prince2002gupta@gmail.com>
2021-09-29 01:38:48 +0530

    Gitacora saved 1 changed files

--- dsaa/stl/stackss/s2.cpp
+++ dsaa/stl/stackss/s2.cpp
@@ -1,4 +1,4 @@
-/*/  parenthesis count balance using stack --------  */
+/*/  parenthesis count balance using stack -------- so  */
  #include<bits/stdc++.h>
 using namespace std;
 unordered_map<int ,int> symbols={{'[',-1},{'{',-2},{'(',-3},{']',1},{'}',2},{')',3}};
