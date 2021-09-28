56e755f02acb28018c264859df63c77c9a8453c1
prince232002 <prince2002gupta@gmail.com>
2021-09-29 01:12:39 +0530

    Gitacora saved 1 changed files

--- dsaa/stl/stackss/s2.cpp
+++ dsaa/stl/stackss/s2.cpp
@@ -1,7 +1,7 @@
 //  parenthesis count balance using stack --------
  #include<bits/stdc++.h>
 using namespace std;
-unordered\\\_map<int ,int> symbols={{'[',-1},{'{',-2},{'(',-3},{']',1},{'}',2},{')',3}};
+multi_map<int ,int> symbols={{'[',-1},{'{',-2},{'(',-3},{']',1},{'}',2},{')',3}};
 string isbalance(string s){
     stack<char> st;
     for(char bracket:s){
