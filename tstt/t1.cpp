014a532c9b4478ce6407a036fa61ff0983d33379
prince232002 <prince2002gupta@gmail.com>
2021-10-07 20:32:16 +0530

    Gitacora saved 1 changed files

--- tstt/t1.cpp
+++ tstt/t1.cpp
@@ -1,17 +1,1 @@
-// #include<iostream>
-// using namespace std;
-// #include<bits/stdc++.h>
-// int main(){
-// //  vector<vector<int>> vec{ { 1, 2, 3 }, 
-// //                          { 4, 5, 6 }, 
-// //                          { 7, 8, 9, 4 } }; 
-// // cout<<vec[2][1];
-//  vector<int> vec[3]{ { 1, 2, 3 }, 
-//                          { 4, 5, 6 }, 
-//                          { 7, 8, 9, 4 } }; 
-// // cout<<vec[2][1];
-// auto c=3;
-// cout<<c;
-
-// return 0;
-// }
+// 
