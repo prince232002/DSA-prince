3eadc233d4888c665a4b6647d21f44da47b00b9e
prince232002 <prince2002gupta@gmail.com>
2021-10-11 00:19:07 +0530

    Gitacora saved 1 changed files

--- tstt/t1.cpp
+++ tstt/t1.cpp
@@ -4,7 +4,3 @@ int main(){
  string  s[s.size()];
  s[2]="d";
 }
-onething that is special about me that i never take anything serious and never take the revenge because 
-i hate to fight and always make try to evolve the matter as possible as it can be get resolved very easily 
-so that's why i don't like to do and 
-i don't want to do 
