dd8376d5159a6e6b2c09b88989174a36713fd3fd
prince232002 <prince2002gupta@gmail.com>
2021-11-08 22:44:34 +0530

    Gitacora saved 1 changed files

--- dsaa/2. string/qu/s5.cpp
+++ dsaa/2. string/qu/s5.cpp
@@ -28,7 +28,7 @@
 // 			int flag = 1;
 
 // 			// Check palindrome                        // k iterates 
-// 			for (int k = 0; k < (j - i + 1) / 2; k++)  //j-1+1 islengthof 
+// 			for (int k = 0; k < (j - i + 1) / 2; k++)  //j-1+1 islengthof substring 
 // 				if (str[i + k] != str[j - k])
 // 					flag = 0;
 
