afa6dee8a2f359b91b14217b2aacc77d3ab0a3a7
prince232002 <prince2002gupta@gmail.com>
2021-12-05 17:03:49 +0530

    Gitacora saved 2 changed files

--- dsaa/2. string/qu/s6.cpp
+++ dsaa/2. string/qu/s6.cpp
@@ -9,10 +9,7 @@ void printSubsequence(string input, string output)
 	// if the input is empty print the output string
 	if (input.empty()) {
 		cout << output << endl;
-		return;
-	}
-      
-	// output is passed with including
+		 is passed with including
 	// the Ist character of
 	// Input string
 	printSubsequence(input.substr(1), output + input[0]);
