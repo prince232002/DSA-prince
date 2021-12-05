f0436458ef5ff31fbeaf57a21e2c084b1c5128ef
prince232002 <prince2002gupta@gmail.com>
2021-12-05 17:03:56 +0530

    Gitacora saved 2 changed files

--- dsaa/2. string/qu/s6.cpp
+++ dsaa/2. string/qu/s6.cpp
@@ -9,7 +9,7 @@ void printSubsequence(string input, string output)
 	// if the input is empty print the output string
 	if (input.empty()) {
 		cout << output << endl;
-		return;
+		
 	}
       
 	// output is passed with including
