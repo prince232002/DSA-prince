323e3779531a9d2e2135677773100cd51011a082
prince232002 <prince2002gupta@gmail.com>
2021-12-06 17:29:07 +0530

    Gitacora saved 2 changed files

--- dsaa/2. string/qu/s9.cpp
+++ dsaa/2. string/qu/s9.cpp
@@ -42,4 +42,4 @@ int main()
 }
 
 // This is code is contributed by rathbhupendra
-// NOTE https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/note
+// NOT https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/note
