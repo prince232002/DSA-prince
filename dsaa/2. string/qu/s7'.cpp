fc6e61037b9c214dd2ad17101f6bf6d0cb22623c
prince232002 <prince2002gupta@gmail.com>
2021-12-07 20:34:12 +0530

    d

---
+++ dsaa/2. string/qu/s7'.cpp
@@ -0,0 +1,51 @@
+/* This problem is just the modification of Longest Common Subsequence problem. The idea is to find the LCS
+(str, str)where str is the input string with the restriction that when both the characters are same, they 
+shouldn’t be on the same index in the two strings. 
+Below is the implementation of the idea. */
+
+// C++ program to find the longest repeating
+// subsequence
+#include <iostream>
+#include <string>
+using namespace std;
+
+// This function mainly returns LCS(str, str)
+// with a condition that same characters at
+// same index are not considered.
+int findLongestRepeatingSubSeq(string str)
+{
+	int n = str.length();
+
+	// Create and initialize DP table
+	int dp[n+1][n+1];
+	for (int i=0; i<=n; i++)
+		for (int j=0; j<=n; j++)
+			dp[i][j] = 0;
+
+	// Fill dp table (similar to LCS loops)
+	for (int i=1; i<=n; i++)
+	{
+		for (int j=1; j<=n; j++)
+		{
+			// If characters match and indexes are
+			// not same
+			if (str[i-1] == str[j-1] && i != j)
+				dp[i][j] = 1 + dp[i-1][j-1];		
+					
+			// If characters do not match
+			else
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+		}
+	}
+	return dp[n][n];
+}
+
+// Driver Program
+int main()
+{
+	string str = "aabb";
+	cout << "The length of the largest subsequence that"
+			" repeats itself is : "
+		<< findLongestRepeatingSubSeq(str);
+	return 0;
+}
--- dsaa/2. string/qu/tempCodeRunnerFile.cpp
+++ dsaa/2. string/qu/tempCodeRunnerFile.cpp
@@ -1,11 +1,2 @@
-#include<bits/stdc++.h>
-using namespace std;
-int check(string a , string b){
-	int n=a.size();
-	string t=a+b;
-	return(t.find(a)!=string::npos);
-}
-int main(){
- string a,b;
- cout<< check("abb","bab");
-}
+
+		// Fix the starting index
