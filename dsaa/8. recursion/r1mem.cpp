b5c74576d4cec3205cfa7812637e2a827b5df1c3
prince232002 <prince2002gupta@gmail.com>
2021-12-28 19:24:11 +0530

    Gitacora saved 5 changed files

--- dsaa/8. recursion/r1mem.cpp
+++ dsaa/8. recursion/r1mem.cpp
@@ -2,14 +2,5 @@
 using namespace std;
 int main(){
 int n=6;
-    int f[10];
-    int mfib(int n)
-    { 
-    if(n<=1) return n;
-    else{
-        if(f[n-2]==-1) f[n-2]=mfib(n-2); 
-        if(f[n-1]==-1) f[n-1]=mfib(n-1); 
-     return f[n-2]+f[n-1];
-    }
-}
+   
 }
