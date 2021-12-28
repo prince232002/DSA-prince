6e9f405b7ffe4ec20803c0341f0fa199cf22a34a
prince232002 <prince2002gupta@gmail.com>
2021-12-28 18:26:40 +0530

    Gitacora saved 5 changed files

--- dsaa/8. recursion/r1.cpp
+++ dsaa/8. recursion/r1.cpp
@@ -13,9 +13,9 @@ cfib(b,c, n-1);
 int main(){
  int a=0,b=1,n;
  cin>>n;
- cout<<a<<endl;
- cout<<b<<endl;
- cfib(a, b, n);
+//  cout<<a<<endl;
+//  cout<<b<<endl;
+//  cfib(a, b, n);
 
 return 0;
 }
