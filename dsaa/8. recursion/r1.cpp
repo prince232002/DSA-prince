2b7e202942ec858ff21ccee669bca4f7037854d8
prince232002 <prince2002gupta@gmail.com>
2021-12-28 19:17:31 +0530

    Gitacora saved 5 changed files

--- dsaa/8. recursion/r1.cpp
+++ dsaa/8. recursion/r1.cpp
@@ -14,15 +14,7 @@ c=a+b;
 cout <<c<<endl;
 cfib(b,c, n-1);
 }
-int mfib(int n){
-    int f[10];
-    if(n<=1) return n;
-    else{
-        if(f[n-2]==-1) f[n-2]=mfib(n-2); 
-        if(f[n-1]==-1) f[n-1]=mfib(n-1); 
-     return f[n-2]+f[n-1];
-    }
-}
+
 int main(){
  int a=0,b=1,n;
  cin>>n;
