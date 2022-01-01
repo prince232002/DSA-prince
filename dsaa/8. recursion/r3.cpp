93b66f1ff013c11af86d6b3c920a5bd22fb71754
prince232002 <prince2002gupta@gmail.com>
2022-01-01 14:51:56 +0530

    Gitacora saved 5 changed files

--- dsaa/8. recursion/r3.cpp
+++ dsaa/8. recursion/r3.cpp
@@ -12,7 +12,6 @@ int power(int x, int n){
     if (n%2==0){   // n is even 
         return power(x, n/2)* power(x, n/2);
         
-    }
     else          //   n is odd 
         return power(x, n/2)* power(x, n/2)*x;
        
