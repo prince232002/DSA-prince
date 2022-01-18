e7ea3a1feaae531758d84cb17017eccc7b3195ca
prince232002 <prince2002gupta@gmail.com>
2022-01-18 23:16:02 +0530

    Gitacora saved 5 changed files

--- placement ds/pointer/p1.cpp
+++ placement ds/pointer/p1.cpp
@@ -2,7 +2,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
- int arr[] = {4, 5, 6, 7};
-  int *p = (arr + 1);
-  cout << arr + 9;
+ int num =4;
+ cout <<num<<endl;
+ //address of opertor 
+ cout << address of num is  
 }
