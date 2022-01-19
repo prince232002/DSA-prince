c88edfd17eeeee46e2031d89ba7dda8b416b48e5
prince232002 <prince2002gupta@gmail.com>
2022-01-19 12:30:48 +0530

    Gitacora saved 5 changed files

--- placement ds/pointer/p1.cpp
+++ placement ds/pointer/p1.cpp
@@ -2,8 +2,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
- int arr[11];
- int *ptr= &arr[0];
+//  int arr[11];
+//  int *ptr= &arr[0];
 //  cout<< sizeof(arr)<<endl;
 //  cout<< sizeof(ptr)<<endl;
 //  cout<< sizeof(*ptr)<<endl;
