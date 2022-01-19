a5836b37a48b05600974684720016ef5290656ab
prince232002 <prince2002gupta@gmail.com>
2022-01-19 12:31:08 +0530

    Gitacora saved 5 changed files

--- placement ds/pointer/p1.cpp
+++ placement ds/pointer/p1.cpp
@@ -10,7 +10,7 @@ int main(){
 // ptr=ptr+1;
 //error :as it is content inside symbol table so unchanged 
 //  arr=arr+1;
-cout << ptr <<endl;
+// cout << ptr <<endl;
 // character arary :
 int arr[]={3,56,7,7};
 char ch[6]="abcde";   // always size is +! as it stores null at end of char array 
