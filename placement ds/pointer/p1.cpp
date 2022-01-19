375358e83a457951c3f9eac0cc4a528367b22eb4
prince232002 <prince2002gupta@gmail.com>
2022-01-19 12:26:44 +0530

    Gitacora saved 5 changed files

--- placement ds/pointer/p1.cpp
+++ placement ds/pointer/p1.cpp
@@ -8,7 +8,7 @@ int main(){
 //  cout<< sizeof(ptr)<<endl;
 //  cout<< sizeof(*ptr)<<endl;
 ptr=ptr+1;
-//error :as it is content inside 
+//error :as it is content inside symbol table 
 //  arr=arr+1;
 cout<< cout<<endl;
 }
